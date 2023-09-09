8ea47e032799a7a0220175429a1e3f7fd7109349
yucel <yakin68@gmail.com>
2023-09-10 01:59:54 +0300

    bookstore

---
+++ main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+variable "secgr-dynamic-ports" {
+  default = [22,80,443]
+}
+
+variable "instance-type" {
+  default = "t2.micro"
+  sensitive = true
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  dynamic "ingress" {
+    for_each = var.secgr-dynamic-ports
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+  egress {
+    description = "Outbound Allowed"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = data.aws_ami.al2023.id
+  instance_type = var.instance-type
+  key_name = "end-key-pair"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  user_data = filebase64("user-data.sh")
+      tags = {
+      Name = "Web Server of Bookstore"
+  }
+}  
+output "myec2-public-ip" {
+  value = aws_instance.tf-ec2.public_ip
+}
---
+++ user-data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yum update -y
+yum install python3 -y
+yum install git -y
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" \
+-o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+cd /home/ec2-user && git clone https://github.com/eakkaya64/bookstore-api.git
+cd /home/ec2-user/bookstore-api && docker-compose up
