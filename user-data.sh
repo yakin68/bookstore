20d329b12ed713d7aaee08450ddd36c1e83e7fbb
Yücel AKIN <yakin68@gmail.com>
2023-09-10 12:41:43 +0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -12,5 +12,5 @@ sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.3/docker
 # sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 \
 # -o /usr/local/bin/docker-compose #bu komutta kullanılabilir
 sudo chmod +x /usr/local/bin/docker-compose
-cd /home/ec2-user && git clone https://github.com/yakin/bookstore.git
+cd /home/ec2-user && git clone https://github.com/yakin68/bookstore.git
 cd /home/ec2-user/bookstore-api && docker-compose up
